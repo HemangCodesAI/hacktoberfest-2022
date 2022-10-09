eef944cb07220d2d8275f78b4895f9ff92915e65
Faizan <faizanalam980@gmail.com>
2022-10-09 10:00:12 +0530

    added the question for N-Queen Problem

--- N-Queen-Problem.cpp
+++ N-Queen-Problem.cpp
@@ -1,3 +1,11 @@
+/*QUESTION IS BELOW
+This problem is to find an arrangement of N queens on a chess board, such that no queen can attack any other queens on the board.
+
+The chess queens can attack in any direction as horizontal, vertical, horizontal and diagonal way.
+
+A binary matrix is used to display the positions of N Queens, where no queens can attack other queens. Here, we solve 8 queens problem.*/
+
+
 /* C++ program to solve N Queen Problem using
    backtracking */
   
