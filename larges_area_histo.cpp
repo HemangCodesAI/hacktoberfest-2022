11e91edbea5826f20ec065dbaa66974c30aa760e
Abhay Chaudhary <chaudharyabhay111@gmail.com>
2022-10-12 15:25:03 +0530

    Updated The description
    Largest Rectangle in Histogram

--- larges_area_histo.cpp
+++ larges_area_histo.cpp
@@ -1,3 +1,5 @@
+/*Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
