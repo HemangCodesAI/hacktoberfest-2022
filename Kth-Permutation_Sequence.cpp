d6bc827571ec41f399ca7039add0922dd829c38e
abhay1822 <72266499+abhay1822@users.noreply.github.com>
2022-10-13 09:29:01 +0530

    update description

--- Kth-Permutation_Sequence.cpp
+++ Kth-Permutation_Sequence.cpp
@@ -1,3 +1,14 @@
+/*The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321" */
+
 #include <bits/stdc++.h>
 
 using namespace std;
