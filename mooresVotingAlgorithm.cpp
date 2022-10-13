f1ca9372961c16622f7b6d81dbf280ce271340a1
Ayush Kumar <55020474+xenacro@users.noreply.github.com>
2022-10-14 01:14:39 +0530

    Fix: include problem statement

--- mooresVotingAlgorithm.cpp
+++ mooresVotingAlgorithm.cpp
@@ -1,3 +1,7 @@
+/*
+Problem Statement: Given an array of N integers, write a program to return an element that occurs more than N/2 times in the given array. You may consider that such an element always exists in the array.
+*/
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
