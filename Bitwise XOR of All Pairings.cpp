f083a93257ca2c0627b573e5fe28e1578c083080
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-10-14 20:33:13 +0530

    Update Bitwise XOR of All Pairings.cpp

--- Bitwise XOR of All Pairings.cpp
+++ Bitwise XOR of All Pairings.cpp
@@ -1,4 +1,22 @@
- #include<bits/stdc++.h>
+Question Statement-
+
+You are given two 0-indexed arrays, nums1 and nums2, consisting of non-negative integers. There exists another array, nums3, 
+which contains the bitwise XOR of all pairings of integers between nums1 and nums2 (every integer in nums1 is paired with every integer in nums2 exactly once).
+Return the bitwise XOR of all integers in nums3.
+
+Example 1:
+
+Input: nums1 = [2,1,3], nums2 = [10,2,5,0]
+Output: 13
+Explanation:
+A possible nums3 array is [8,0,7,2,11,3,4,1,9,1,6,3].
+The bitwise XOR of all these numbers is 13, so we return 13.
+
+
+
+Code:
+
+#include<bits/stdc++.h>
  using namespace std;
   int xorAllNums(vector<int> &nums1, vector<int> &nums2)
         {
