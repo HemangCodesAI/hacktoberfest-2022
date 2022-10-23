b2ed6ba898747e8ec951a2e7f1ace2434dbc4d65
Aneesh Sharma <54682356+aneeshsharma0671@users.noreply.github.com>
2022-10-23 20:39:40 +0530

    Update Dijkstra.cpp
    Added problem Statement

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,3 +1,7 @@
+//Problem Statement: Given a weighted, undirected, and connected graph of V vertices and E edges
+//Find the shortest distance of all the vertex’s from the source vertex S.
+//Note: The Graph doesn’t contain any negative weight cycle.
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
