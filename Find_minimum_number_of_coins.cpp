79303b161e10d0f6aab23e6863edff5cfe676dd8
pkpk1987 <90591476+pkpk1987@users.noreply.github.com>
2022-10-23 23:48:51 +0530

    Find_minimum_number_of_coins.cpp
    Problem Statement: Given a value V, if we want to make a change for V Rs, and we have an infinite supply of each of the denominations in Indian currency, i.e., we have an infinite supply of { 1, 2, 5, 10, 20, 50, 100, 500, 1000} valued coins/notes, what is the minimum number of coins and/or notes needed to make the change.
    
    Example 1:
    
    Input: V = 70
    
    Output: 2
    
    Explaination: We need a 50 Rs note and a 20 Rs note.
    
    Example 2:
    
    Input: V = 121
    
    Output: 3
    
    Explaination: We need a 100 Rs note, a 20 Rs note and a 1 Rs coin.
    
    Signed-off-by: pkpk1987 <90591476+pkpk1987@users.noreply.github.com>

---
+++ Find_minimum_number_of_coins.cpp
@@ -0,0 +1,41 @@
+// Problem Statement: Given a value V, if we want to make a change for V Rs, and we have an infinite supply of each of the denominations in Indian currency, i.e., we have an infinite supply of { 1, 2, 5, 10, 20, 50, 100, 500, 1000} valued coins/notes, what is the minimum number of coins and/or notes needed to make the change.
+
+// Example 1:
+
+// Input: V = 70
+
+// Output: 2
+
+// Explaination: We need a 50 Rs note and a 20 Rs note.
+
+// Example 2:
+
+// Input: V = 121
+
+// Output: 3
+
+// Explaination: We need a 100 Rs note, a 20 Rs note and a 1 Rs coin.
+
+#include<bits/stdc++.h>
+
+
+using namespace std;
+int main() {
+  int V = 49;
+  vector < int > ans;
+  int coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+  int n = 9;
+  for (int i = n - 1; i >= 0; i--) {
+    while (V >= coins[i]) {
+      V -= coins[i];
+      ans.push_back(coins[i]);
+    }
+  }
+  cout<<"The minimum number of coins is "<<ans.size()<<endl;
+  cout<<"The coins are "<<endl;
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << " ";
+  }
+
+  return 0;
+}
