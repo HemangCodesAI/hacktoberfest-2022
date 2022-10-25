d8cfa0f7169f3bc3e270790e457b807c61e61f54
Apeksha Verma <59526735+Apeksha16@users.noreply.github.com>
2022-10-25 23:17:25 +0530

    Rotate_Image_by_90_degree.cpp
    Problem Statement: Given a matrix, your task is to rotate the matrix 90 degrees clockwise.
    
    
    
    Signed-off-by: Apeksha Verma <59526735+Apeksha16@users.noreply.github.com>

---
+++ Rotate_Image_by_90_degree.cpp
@@ -0,0 +1,47 @@
+// Problem Statement: Given a matrix, your task is to rotate the matrix 90 degrees clockwise.
+
+// Examples:
+
+// Example 1:
+
+// Input: [[1,2,3],[4,5,6],[7,8,9]]
+
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+// Explanation: Rotate the matrix simply by 90 degree clockwise and return the matrix.
+
+// Example 2:
+
+// Input: [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+
+// Output:[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+// Explanation: Rotate the matrix simply by 90 degree clockwise and return the matrix
+
+#include<bits/stdc++.h>
+
+using namespace std;
+vector < vector < int >> rotate(vector < vector < int >> & matrix) {
+  int n = matrix.size();
+  vector < vector < int >> rotated(n, vector < int > (n, 0));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      rotated[j][n - i - 1] = matrix[i][j];
+    }
+  }
+  return rotated;
+}
+
+int main() {
+  vector < vector < int >> arr;
+  arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  vector < vector < int >> rotated = rotate(arr);
+  cout << "Rotated Image" << endl;
+  for (int i = 0; i < rotated.size(); i++) {
+    for (int j = 0; j < rotated[0].size(); j++) {
+      cout << rotated[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+}
