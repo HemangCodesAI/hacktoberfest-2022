257dc015e20dfed6a364524d37148b48e715badd
Hemang Joshi <hemanghades@gmail.com>
2022-10-27 14:57:44 +0530

    Update Josephus_Problem.cpp
    Signed-off-by: Hemang Joshi <hemanghades@gmail.com>

--- Josephus_Problem.cpp
+++ Josephus_Problem.cpp
@@ -1,11 +1,16 @@
-// Problem Statement: There are n people standing in a circle waiting to be executed. The counting out begins at some point in the circle and proceeds around the circle in a fixed direction. In each step, a certain number of people are skipped and the next person is executed. The elimination proceeds around the circle (which is becoming smaller and smaller as the executed people are removed), until only the last person remains, who is given freedom. Given the total number of person n and a number k which indicates that k-1 persons are skipped and the kth person is killed in a circle. The task is to choose the place in the initial circle so that you are the last one remaining and so survive. 
+// Problem Statement: There are n people standing in a circle waiting to be executed. The counting out begins at some point in the circle and 
+//proceeds around the circle in a fixed direction. In each step, a certain number of people are skipped and the next person is executed. The elimination 
+//proceeds around the circle (which is becoming smaller and smaller as the executed people are removed), until only the last person remains, who is given freedom.
+//Given the total number of person n and a number k which indicates that k-1 persons are skipped and the kth person is killed in a circle. The task is to choose 
+//the place in the initial circle so that you are the last one remaining and so survive. 
 
 // Examples:
 
 // Example 1:
 // Input: n = 5 and k = 2
 // Output: The safe position is 3
-// Explanation:  Firstly, the person at position 2 is killed, then person at position 4 is killed, then person at position 1 is killed. Finally, the person at position 5 is killed. So the person at position 3 survives. 
+// Explanation:  Firstly, the person at position 2 is killed, then person at position 4 is killed, then person at position 1 is killed. Finally, the person at 
+//position 5 is killed. So the person at position 3 survives. 
 
 // Example 2:
 // Input:  n = 7 and k = 3
